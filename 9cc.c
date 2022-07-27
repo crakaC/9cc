021716a7c200df875d5763f886f795f2b113ce84
Kosuke SHIRAKASHI <k.shira86@gmail.com>
2022-07-27 20:57:24 +0900

    Implement mul and div.

--- 9cc.c
+++ 9cc.c
@@ -25,9 +25,9 @@ struct Token {
 // 現在着目しているトークン
 Token* token;
 
-char *user_input;
+char* user_input;
 
-void error_at(char *loc, char *fmt, ...){
+void error_at(char* loc, char* fmt, ...) {
     va_list ap;
     va_start(ap, fmt);
     int pos = loc - user_input;
@@ -92,7 +92,7 @@ Token* tokenize(char* p) {
             p++;
             continue;
         }
-        if (*p == '+' || *p == '-') {
+        if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')') {
             cur = new_token(TK_RESERVED, cur, p++);
             continue;
         }
@@ -107,6 +107,111 @@ Token* tokenize(char* p) {
     return head.next;
 }
 
+// 抽象構文木のノードの種類
+typedef enum {
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_MUL, // *
+    ND_DIV, // /
+    ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+    NodeKind kind;
+    Node* lhs; // 左辺
+    Node* rhs; // 右辺
+    int val; // kindがND_NUMのときに使う
+};
+
+Node* new_node(NodeKind kind, Node* lhs, Node* rhs) {
+    Node* node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node* new_node_num(int val) {
+    Node* node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node* expr();
+Node* mul();
+Node* primary();
+
+Node* expr() {
+    Node* node = mul();
+    for (;;) {
+        if (consume('+')) {
+            node = new_node(ND_ADD, node, mul());
+        } else if (consume('-')) {
+            node = new_node(ND_SUB, node, mul());
+        } else {
+            return node;
+        }
+    }
+}
+
+Node* mul() {
+    Node* node = primary();
+    for (;;) {
+        if (consume('*')) {
+            node = new_node(ND_MUL, node, primary());
+        } else if (consume('/')) {
+            node = new_node(ND_DIV, node, primary());
+        } else {
+            return node;
+        }
+    }
+}
+
+Node* primary() {
+    // 次のトークンが'('なら、'(' expr ')'のはず
+    if (consume('(')) {
+        Node* node = expr();
+        expect(')');
+        return node;
+    }
+
+    // そうでなければ数値のはず
+    return new_node_num(expect_number());
+}
+
+void gen(Node* node) {
+    if (node->kind == ND_NUM) {
+        printf("  push %d\n", node->val);
+        return;
+    }
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+
+    switch (node->kind) {
+    case ND_ADD:
+        printf("  add rax, rdi\n");
+        break;
+    case ND_SUB:
+        printf("  sub rax, rdi\n");
+        break;
+    case ND_MUL:
+        printf("  imul rax, rdi\n");
+        break;
+    case ND_DIV:
+        printf("  cqo\n");
+        printf("  idiv rdi\n");
+        break;
+    }
+
+    printf("  push rax\n");
+}
+
 int main(int argc, char** argv) {
     if (argc != 2) {
         fprintf(stderr, "invalid number of arguments\n");
@@ -115,24 +220,15 @@ int main(int argc, char** argv) {
 
     user_input = argv[1];
     token = tokenize(argv[1]);
-
+    Node* node = expr();
 
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
 
-    // 式の最初は数でなければならないのでそれをチェックして最初のmov命令を出力
-    printf("  mov rax, %d\n", expect_number());
+    gen(node);
 
-    while (!at_eof()) {
-        if (consume('+')) {
-            printf("  add rax, %d\n", expect_number());
-            continue;
-        }
-        expect('-');
-        printf("  sub rax, %d\n", expect_number());
-    }
-
+    printf("  pop rax\n");
     printf("  ret\n");
     return 0;
 }
